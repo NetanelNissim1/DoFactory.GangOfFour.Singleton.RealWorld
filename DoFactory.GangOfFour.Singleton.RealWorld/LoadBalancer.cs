ba3590fb7a3d8893f45e741e36ea9e0607274dd2
Nati <nati.nissim@gmail.com>
2017-11-22 12:13:09 +0200

    First

---
+++ DoFactory.GangOfFour.Singleton.RealWorld/LoadBalancer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoFactory.GangOfFour.Singleton.RealWorld
+{
+    class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private Random _random = new Random();
+        private List<Server> _servers;
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        // Constructor (protected)
+        protected LoadBalancer()
+        {
+            // Load list of available servers
+            _servers = new List<Server>
+        {
+         new Server{ Name = "ServerI  ", IP = "120.14.220.18" },
+         new Server{ Name = "ServerII ", IP = "120.14.220.19" },
+         new Server{ Name = "ServerIII", IP = "120.14.220.20" },
+         new Server{ Name = "ServerIV ", IP = "120.14.220.21" },
+         new Server{ Name = "ServerV  ", IP = "120.14.220.22" },
+        };
+
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            // Support multithreaded applications through
+            // 'Double checked locking' pattern which (once
+            // the instance exists) avoids locking each
+            // time the method is invoked
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LoadBalancer();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        // Simple, but effective random load balancer
+        /// <summary>
+        /// 
+        /// </summary>
+        public Server NextServer
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r];
+            }
+        }
+    }
+}
--- DoFactory.GangOfFour.Singleton.RealWorld/Program.cs
+++ DoFactory.GangOfFour.Singleton.RealWorld/Program.cs
@@ -10,6 +10,28 @@ namespace DoFactory.GangOfFour.Singleton.RealWorld
     {
         static void Main(string[] args)
         {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string serverName = balancer.NextServer.Name;
+                string serverIp = balancer.NextServer.IP;
+                Console.WriteLine("Dispatch Request to: {0} :  Ip: {1}", serverName, serverIp);
+            }
+
+            // Wait for user
+            Console.ReadKey();
         }
     }
 }
